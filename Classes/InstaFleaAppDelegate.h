4697867879877fb7fcc63018da257496bd6d6c93
Edward Ocampo-Gooding <edward@edwardog.net>
2010-11-27 14:27:53 -0500

    First commit

---
+++ Classes/InstaFleaAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  InstaFleaAppDelegate.h
+//  InstaFlea
+//
+//  Created by Edward Ocampo-Gooding on 10-11-27.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface InstaFleaAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  InstaFlea
+//
+//  Created by Edward Ocampo-Gooding on 10-11-27.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"InstaFleaAppDelegate");
+    [pool release];
+    return retVal;
+}
